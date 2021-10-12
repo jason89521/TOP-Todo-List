ce4ae42de1280c1214edce67574d34e525a358f5
jason89521 <97ssps30212@gmail.com>
2021-10-12 23:53:58 +0800

    Edit task

--- src/listContainer.js
+++ src/listContainer.js
@@ -10,18 +10,40 @@ const form = document.getElementById('task-form');
 // Initialize
 form.remove();
 form.addEventListener('keydown', e => {
-    if(e.target.tagName.toLowerCase() === 'input' && e.key.toLowerCase() === 'enter') 
+    if (e.target.tagName.toLowerCase() === 'input' && e.key.toLowerCase() === 'enter')
         e.preventDefault();
 });
 resetTaskList(taskList, addTaskLi, 'Inbox');
 
 taskList.addEventListener('click', async e => {
     const action = e.target.dataset.action;
-    if(action === 'delete') {
+    if (action === 'delete') {
         const liToBeDeleted = getLiFromChild(e.target);
         liToBeDeleted.remove();
         const id = parseInt(liToBeDeleted.dataset.id, 10);
         deleteItem(tasksKey, value => value.id === id);
+    } else if (action === 'edit') {
+        const liToBeEdited = getLiFromChild(e.target);
+        const swalResult = await Swal.fire({
+            html: form,
+            preConfirm: () => {
+                const elements = form.elements;
+                if (!(/\S/.test(elements['title'].value)))
+                    Swal.showValidationMessage('Please enter the title!');
+            }
+        });
+        if (swalResult.value) {
+            const title = form.elements['title'].value;
+            const description = form.elements['description'].value;
+            const date = form.elements['date'].value;
+            const priority = form.elements['priority'].value;
+            const projectName = projectTitle.innerText;
+            const id = parseInt(liToBeEdited.dataset.id, 10);
+            const task = new Task(title, description, date, priority, projectName, id);
+            taskList.insertBefore(task.createTaskElement(), liToBeEdited);
+            liToBeEdited.remove();
+            updateItem(tasksKey, value => value.id === id, task);
+        }
     }
 });
 
@@ -30,12 +52,12 @@ addTaskLi.addEventListener('click', async e => {
         html: form,
         preConfirm: () => {
             const elements = form.elements;
-            if(!(/\S/.test(elements['title'].value)))
+            if (!(/\S/.test(elements['title'].value)))
                 Swal.showValidationMessage('Please enter the title!');
         }
     })
-    
-    if(swalResult.value) {
+
+    if (swalResult.value) {
         const title = form.elements['title'].value;
         const description = form.elements['description'].value;
         const date = form.elements['date'].value;
@@ -52,7 +74,7 @@ addTaskLi.addEventListener('click', async e => {
  */
 function getLiFromChild(child) {
     let li = child;
-    while(li.tagName.toLowerCase() !== 'li') {
+    while (li.tagName.toLowerCase() !== 'li') {
         li = li.parentNode;
     }
     return li
